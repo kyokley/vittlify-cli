dde1eedace790f131f3553e80bbdeef7ed55b387
Kevin Yokley <kyokley2@gmail.com>
2017-06-25 20:09:32 -0500

    Adding CLI args

--- vt/vt.py
+++ vt/vt.py
@@ -1,11 +1,15 @@
+import argparse
 import requests
 import json
+import sys
 #import os
 import base64
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import padding
 #from cryptography.exceptions import InvalidSignature
+
+from colorclass import Color
 
 from terminaltables import AsciiTable
 from pprint import pprint
@@ -71,12 +75,12 @@ def display_shopping_list(guid, show_done=False):
     data = []
     for item in _get_shopping_list_items(guid):
         if (not show_done and not item['done']) or show_done:
-            data.append([item['guid'][:8],
+            data.append([Color('{autoblue}%(guid)s{/autoblue}' % {'guid': item['guid'][:8]}),
                          item['name']])
 
     if data:
         table = AsciiTable(data)
-        table.title = shopping_list['name']
+        table.title = Color("{autoyellow}%(name)s{/autoyellow}" % {'name': shopping_list['name']})
         table.inner_heading_row_border = False
         print table.table
 
@@ -84,12 +88,12 @@ def display_all_shopping_lists():
     shopping_lists = _get_all_shopping_lists()
     data = []
     for shopping_list in shopping_lists:
-        data.append([shopping_list['guid'][:8],
+        data.append([Color('{autoblue}%(guid)s{/autoblue}' % {'guid': shopping_list['guid'][:8]}),
                      shopping_list['name']])
 
     if data:
         table = AsciiTable(data)
-        table.title = 'All Lists'
+        table.title = Color("{autoyellow}%(name)s{/autoyellow}" % {'name': 'All Lists'})
         table.inner_heading_row_border = False
         print table.table
 
@@ -100,7 +104,14 @@ def _get_encoded_signature(message):
                             hashes.SHA512())
     return base64.b64encode(signature)
 
+def main():
+    if sys.argv[1].lower() == 'show':
+        if sys.argv[2].lower() == 'list':
+            if len(sys.argv) != 4:
+                raise ValueError('Incorrect number of arguments')
+            display_shopping_list(sys.argv[3])
+        elif sys.argv[2].lower() == 'lists':
+            display_all_shopping_lists()
+
 if __name__ == '__main__':
-    #get_items('work')
-    #display_all_shopping_lists()
-    display_shopping_list('162d4770')
+    main()
