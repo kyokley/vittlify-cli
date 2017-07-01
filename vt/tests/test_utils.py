e01f3d23008bf2405b444dc3afb6358de2f5977f
Kevin Yokley <kyokley2@gmail.com>
2017-07-01 12:11:28 -0500

    Adding tests for print_table method

--- vt/tests/test_utils.py
+++ vt/tests/test_utils.py
@@ -1,7 +1,7 @@
 import unittest
 import mock
 
-from vt.utils import format_row
+from vt.utils import format_row, print_table
 
 class TestFormatRow(unittest.TestCase):
     def setUp(self):
@@ -95,3 +95,33 @@ class TestFormatRow(unittest.TestCase):
         self.assertEqual(expected, actual)
         self.mock_Color.assert_has_calls([mock.call('{strike}{autoblue}asdf{/autoblue}{/strike}'),
                                           mock.call('{strike}{automagenta}+ test_name{/automagenta}{/strike}')])
+
+class TestPrintTable(unittest.TestCase):
+    def setUp(self):
+        self.Color_patcher = mock.patch('vt.utils.Color')
+        self.mock_Color = self.Color_patcher.start()
+
+        self.AsciiTable_patcher = mock.patch('vt.utils.AsciiTable')
+        self.mock_AsciiTable = self.AsciiTable_patcher.start()
+
+    def tearDown(self):
+        self.Color_patcher.stop()
+        self.AsciiTable_patcher.stop()
+
+    def test_no_data(self):
+        print_table([])
+        self.mock_Color.assert_called_once_with("{autored}No data found.{/autored}")
+
+    def test_data_no_title(self):
+        print_table(['test_data'])
+
+        self.mock_AsciiTable.assert_called_once_with(['test_data'])
+        self.assertEqual(self.mock_AsciiTable.return_value.inner_heading_row_border, False)
+
+    def test_data_with_title(self):
+        print_table(['test_data'], title='test_title')
+
+        self.mock_AsciiTable.assert_called_once_with(['test_data'])
+        self.assertEqual(self.mock_AsciiTable.return_value.inner_heading_row_border, False)
+        self.mock_Color.assert_called_once_with("{autoyellow}test_title{/autoyellow}")
+        self.assertEqual(self.mock_AsciiTable.return_value.title, self.mock_Color.return_value)
