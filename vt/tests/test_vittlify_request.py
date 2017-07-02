f877cd67199d3b604a33fd6e2d05a09e519128a6
Kevin Yokley <kyokley2@gmail.com>
2017-07-02 16:45:05 -0500

    Adding tests for requests

--- vt/tests/test_vittlify_request.py
+++ vt/tests/test_vittlify_request.py
@@ -8,6 +8,8 @@ from vt.vittlify_request import (_send_request,
                                  get_all_shopping_list_items,
                                  get_completed,
                                  get_item,
+                                 add_item,
+                                 modify_item,
                                  complete_item,
                                  )
 
@@ -232,3 +234,54 @@ class TestCompleteItem(unittest.TestCase):
         self.mock_send_request.assert_called_once_with('PUT', {'endpoint': 'uncomplete',
                                                                'username': 'USERNAME',
                                                                'guid': test_guid})
+
+class TestModifyItem(unittest.TestCase):
+    def setUp(self):
+        self.USERNAME_patcher = mock.patch('vt.vittlify_request.USERNAME', 'USERNAME')
+        self.USERNAME_patcher.start()
+
+        self._send_request_patcher = mock.patch('vt.vittlify_request._send_request')
+        self.mock_send_request = self._send_request_patcher.start()
+
+    def tearDown(self):
+        self.USERNAME_patcher.stop()
+        self._send_request_patcher.stop()
+
+    def test_(self):
+        test_guid = 'test_guid'
+        comments = 'test_comments'
+        expected = self.mock_send_request.return_value
+        actual = modify_item(test_guid, comments)
+
+        self.assertEqual(expected, actual)
+        self.mock_send_request.assert_called_once_with('PUT', {'endpoint': 'modify',
+                                                               'username': 'USERNAME',
+                                                               'guid': test_guid,
+                                                               'comments': comments})
+
+class TestAddItem(unittest.TestCase):
+    def setUp(self):
+        self.USERNAME_patcher = mock.patch('vt.vittlify_request.USERNAME', 'USERNAME')
+        self.USERNAME_patcher.start()
+
+        self._send_request_patcher = mock.patch('vt.vittlify_request._send_request')
+        self.mock_send_request = self._send_request_patcher.start()
+
+    def tearDown(self):
+        self.USERNAME_patcher.stop()
+        self._send_request_patcher.stop()
+
+    def test_(self):
+        test_guid = 'test_guid'
+        comments = 'test_comments'
+        name = 'test_name'
+
+        expected = self.mock_send_request.return_value
+        actual = add_item(test_guid, name, comments=comments)
+
+        self.assertEqual(expected, actual)
+        self.mock_send_request.assert_called_once_with('POST', {'endpoint': 'add item',
+                                                                'username': 'USERNAME',
+                                                                'guid': test_guid,
+                                                                'comments': comments,
+                                                                'name': name})
