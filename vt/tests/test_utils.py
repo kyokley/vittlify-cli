19c96477fb08c034ad109fac06d7b8ba9212a637
Kevin Yokley <kyokley2@gmail.com>
2017-07-23 08:55:08 -0500

    Adding tests

--- vt/tests/test_utils.py
+++ vt/tests/test_utils.py
@@ -1,10 +1,16 @@
+import os
+import sys
 import unittest
 import mock
 
 from vt.utils import (format_row,
                       print_table,
                       parse_options,
+                      apply_strikethrough,
+                      get_encoded_signature,
                       )
+
+PY3 = sys.version_info[0] == 3
 
 class TestFormatRow(unittest.TestCase):
     def setUp(self):
@@ -288,3 +294,80 @@ class TestParseOptions(unittest.TestCase):
         actual = parse_options(raw_options)
 
         self.assertEqual(expected, actual)
+
+class TestApplyStrikethrough(unittest.TestCase):
+    def test_plain_string(self):
+        test_str = 'string'
+        expected = '{strike}string{/strike}'
+
+        actual = apply_strikethrough(test_str)
+        self.assertEqual(expected, actual)
+
+    def test_plain_string_with_spaces(self):
+        test_str = 'this is a string'
+        expected = '{strike}this{/strike} {strike}is{/strike} {strike}a{/strike} {strike}string{/strike}'
+
+        actual = apply_strikethrough(test_str)
+        self.assertEqual(expected, actual)
+
+    def test_skip_color_tags(self):
+        test_str = 'this is a {yellow}string{/yellow}'
+        expected = '{strike}this{/strike} {strike}is{/strike} {strike}a{/strike} {strike}{yellow}string{/yellow}{/strike}'
+
+        actual = apply_strikethrough(test_str)
+        self.assertEqual(expected, actual)
+
+class TestGetEncodedSignature(unittest.TestCase):
+    def setUp(self):
+        mock_environ = dict(os.environ)
+        mock_environ['VT_PRIVATE_KEY'] = 'test_key_location'
+
+        self.environ_patcher = mock.patch('vt.utils.os.environ', mock_environ)
+        self.environ_patcher.start()
+
+        self.open_patcher = mock.patch('builtins.open' if PY3 else '__builtin__.open')
+        self.mock_open = self.open_patcher.start()
+
+        self.b64encode_patcher = mock.patch('vt.utils.base64.b64encode')
+        self.mock_b64encode = self.b64encode_patcher.start()
+
+        self.load_pem_private_key_patcher = mock.patch('vt.utils.serialization.load_pem_private_key')
+        self.mock_load_pem_private_key = self.load_pem_private_key_patcher.start()
+
+        self.default_backend_patcher = mock.patch('vt.utils.default_backend')
+        self.mock_default_backend = self.default_backend_patcher.start()
+
+        self.padding_patcher = mock.patch('vt.utils.padding')
+        self.mock_padding = self.padding_patcher.start()
+
+        self.hashes_patcher = mock.patch('vt.utils.hashes')
+        self.mock_hashes = self.hashes_patcher.start()
+
+    def tearDown(self):
+        self.environ_patcher.stop()
+        self.open_patcher.stop()
+        self.b64encode_patcher.stop()
+        self.load_pem_private_key_patcher.stop()
+        self.default_backend_patcher.stop()
+        self.padding_patcher.stop()
+        self.hashes_patcher.stop()
+
+    def test_(self):
+        test_message = 'tests message'
+
+        expected = self.mock_b64encode.return_value
+        actual = get_encoded_signature(test_message)
+
+        self.assertEqual(expected, actual)
+        self.mock_b64encode.assert_called_once_with(self.mock_load_pem_private_key.return_value.sign.return_value)
+        self.mock_load_pem_private_key.assert_called_once_with(self.mock_open.return_value.__enter__.return_value.read.return_value,
+                                                               None,
+                                                               self.mock_default_backend.return_value)
+
+        self.mock_load_pem_private_key.return_value.sign.assert_called_once_with(test_message,
+                                                                                 self.mock_padding.PSS.return_value,
+                                                                                 self.mock_hashes.SHA512.return_value
+                                                                                 )
+        self.mock_padding.PSS.assert_called_once_with(mgf=self.mock_padding.MGF1.return_value,
+                                                      salt_length=self.mock_padding.PSS.MAX_LENGTH)
+        self.mock_padding.MGF1.assert_called_once_with(self.mock_hashes.SHA512.return_value)
